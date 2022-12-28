e0e664a081bf460d3bbb0bb448f531de176ed0fb
sekhyuni <sekhyuni@gmail.com>
2022-12-28 23:31:18 +0900

    feat: 프로그래머스 / Lv. 2 / 올바른 괄호 문제 추가

---
+++ programmers/stack/correct-bracket.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    const stack = [];
+    const lengthOfS = s.length;
+
+    stack.push(s[0]);
+    for (let i = 1; i < lengthOfS; i++) {
+        if (stack[stack.length - 1] === '(' && s[i] === ')') {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+
+    return !stack.length;
+}
+
+console.log(solution('(())()'))
