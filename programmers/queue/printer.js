31a36d4ff25d557e13e2cfcea5d1611e563c65d9
sekhyuni <sekhyuni@gmail.com>
2022-12-30 21:04:04 +0900

    feat: 프로그래머스 / Lv. 2 / 프린터 문제 추가

---
+++ programmers/queue/printer.js
@@ -0,0 +1,24 @@
+function solution(priorities, location) {
+    let answer = 0;
+    const prioritiesOfObject = priorities.map((priority, idx) => ({ priority, isMyOrder: idx === location }))
+
+    while (prioritiesOfObject.length > 0) {
+        const maxPriority = [...prioritiesOfObject].sort((a, b) => b.priority - a.priority)[0].priority;
+        const current = prioritiesOfObject.shift();
+
+        if (current.priority < maxPriority) {
+            prioritiesOfObject.push(current);
+        } else {
+            answer++;
+
+            if (current.isMyOrder) {
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution([2, 1, 3, 2], 2));
+console.log(solution([1, 1, 9, 1, 1, 1], 0));
