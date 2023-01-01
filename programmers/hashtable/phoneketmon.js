7bf143c773e8874b47849fc998b9864423fa1759
sekhyuni <sekhyuni@gmail.com>
2023-01-01 12:58:13 +0900

    feat: 프로그래머스 / Lv. 1 / 폰켓몬 문제 추가

---
+++ programmers/hashtable/phoneketmon.js
@@ -0,0 +1,10 @@
+function solution(nums) {
+    const max = nums.length / 2;
+    const setOfPhonekeymon = new Set(nums);
+
+    return setOfPhonekeymon.size > max ? max : setOfPhonekeymon.size;
+}
+
+console.log(solution([3, 1, 2, 3]));
+console.log(solution([3, 3, 3, 2, 2, 4]));
+console.log(solution([3, 3, 3, 2, 2, 2]));
