4df064a35dd55234f7cbc24068401535d72fd7fb
sekhyuni <sekhyuni@gmail.com>
2025-11-20 15:24:32 +0900

    feat: delete node in a linked list

---
+++ data-structure/singly-linked-list/leetcode/medium/delete-node-in-a-linked-list.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(node: ListNode | null): void {
+  if (node === null || node.next === null) return;
+  node.val = node.next.val;
+  node.next = node.next.next;
+}
